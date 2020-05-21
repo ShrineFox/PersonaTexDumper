5d3905328ff34715d469da0c23b2149df66ecab4
Ryan ShrineFox <ShrineFox@users.noreply.github.com>
2020-05-20 23:04:37 -0400

    Initial commit

---
+++ PersonaTexDumper/PersonaTexDumper/PersonaTexDumperForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace PersonaTexDumper
+{
+    partial class Form_PersonaTextDumper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_PersonaTextDumper));
+            this.btn_Dump = new System.Windows.Forms.Button();
+            this.btn_BrowseSearch = new System.Windows.Forms.Button();
+            this.txtBox_SearchDir = new System.Windows.Forms.TextBox();
+            this.lbl_SearchDir = new System.Windows.Forms.Label();
+            this.comboBox_Type = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkBox_KeepFolderStructure = new System.Windows.Forms.CheckBox();
+            this.chkBox_NameAfterContainers = new System.Windows.Forms.CheckBox();
+            this.lbl_OutputDir = new System.Windows.Forms.Label();
+            this.txtBox_OutputDir = new System.Windows.Forms.TextBox();
+            this.btn_BrowseOutput = new System.Windows.Forms.Button();
+            this.txtBox_Log = new System.Windows.Forms.RichTextBox();
+            this.chkBox_ExtractPAC = new System.Windows.Forms.CheckBox();
+            this.chkBox_SearchSubfolders = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btn_Dump
+            // 
+            this.btn_Dump.Location = new System.Drawing.Point(310, 239);
+            this.btn_Dump.Name = "btn_Dump";
+            this.btn_Dump.Size = new System.Drawing.Size(75, 107);
+            this.btn_Dump.TabIndex = 11;
+            this.btn_Dump.Text = "Dump";
+            this.btn_Dump.UseVisualStyleBackColor = true;
+            this.btn_Dump.Click += new System.EventHandler(this.btn_Dump_Click);
+            // 
+            // btn_BrowseSearch
+            // 
+            this.btn_BrowseSearch.Location = new System.Drawing.Point(310, 55);
+            this.btn_BrowseSearch.Name = "btn_BrowseSearch";
+            this.btn_BrowseSearch.Size = new System.Drawing.Size(75, 30);
+            this.btn_BrowseSearch.TabIndex = 3;
+            this.btn_BrowseSearch.Text = "Browse...";
+            this.btn_BrowseSearch.UseVisualStyleBackColor = true;
+            this.btn_BrowseSearch.Click += new System.EventHandler(this.btn_BrowseSearch_Click);
+            // 
+            // txtBox_SearchDir
+            // 
+            this.txtBox_SearchDir.Location = new System.Drawing.Point(12, 59);
+            this.txtBox_SearchDir.Name = "txtBox_SearchDir";
+            this.txtBox_SearchDir.Size = new System.Drawing.Size(292, 22);
+            this.txtBox_SearchDir.TabIndex = 2;
+            // 
+            // lbl_SearchDir
+            // 
+            this.lbl_SearchDir.AutoSize = true;
+            this.lbl_SearchDir.Location = new System.Drawing.Point(12, 39);
+            this.lbl_SearchDir.Name = "lbl_SearchDir";
+            this.lbl_SearchDir.Size = new System.Drawing.Size(118, 17);
+            this.lbl_SearchDir.TabIndex = 0;
+            this.lbl_SearchDir.Text = "Search Directory:";
+            // 
+            // comboBox_Type
+            // 
+            this.comboBox_Type.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Type.FormattingEnabled = true;
+            this.comboBox_Type.Items.AddRange(new object[] {
+            "Any",
+            "RMD",
+            "GMD",
+            "EPL",
+            "SPR",
+            "SPD",
+            "TMX"});
+            this.comboBox_Type.Location = new System.Drawing.Point(127, 12);
+            this.comboBox_Type.Name = "comboBox_Type";
+            this.comboBox_Type.Size = new System.Drawing.Size(258, 24);
+            this.comboBox_Type.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Container Type:";
+            // 
+            // chkBox_KeepFolderStructure
+            // 
+            this.chkBox_KeepFolderStructure.AutoSize = true;
+            this.chkBox_KeepFolderStructure.Checked = true;
+            this.chkBox_KeepFolderStructure.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBox_KeepFolderStructure.Location = new System.Drawing.Point(14, 186);
+            this.chkBox_KeepFolderStructure.Name = "chkBox_KeepFolderStructure";
+            this.chkBox_KeepFolderStructure.Size = new System.Drawing.Size(222, 21);
+            this.chkBox_KeepFolderStructure.TabIndex = 8;
+            this.chkBox_KeepFolderStructure.Text = "Keep Original Folder Structure";
+            this.chkBox_KeepFolderStructure.UseVisualStyleBackColor = true;
+            // 
+            // chkBox_NameAfterContainers
+            // 
+            this.chkBox_NameAfterContainers.AutoSize = true;
+            this.chkBox_NameAfterContainers.Checked = true;
+            this.chkBox_NameAfterContainers.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBox_NameAfterContainers.Location = new System.Drawing.Point(14, 213);
+            this.chkBox_NameAfterContainers.Name = "chkBox_NameAfterContainers";
+            this.chkBox_NameAfterContainers.Size = new System.Drawing.Size(323, 21);
+            this.chkBox_NameAfterContainers.TabIndex = 9;
+            this.chkBox_NameAfterContainers.Text = "Make Folders Named After Original Containers";
+            this.chkBox_NameAfterContainers.UseVisualStyleBackColor = true;
+            // 
+            // lbl_OutputDir
+            // 
+            this.lbl_OutputDir.AutoSize = true;
+            this.lbl_OutputDir.Location = new System.Drawing.Point(12, 84);
+            this.lbl_OutputDir.Name = "lbl_OutputDir";
+            this.lbl_OutputDir.Size = new System.Drawing.Size(116, 17);
+            this.lbl_OutputDir.TabIndex = 0;
+            this.lbl_OutputDir.Text = "Output Directory:";
+            // 
+            // txtBox_OutputDir
+            // 
+            this.txtBox_OutputDir.Location = new System.Drawing.Point(12, 104);
+            this.txtBox_OutputDir.Name = "txtBox_OutputDir";
+            this.txtBox_OutputDir.Size = new System.Drawing.Size(292, 22);
+            this.txtBox_OutputDir.TabIndex = 4;
+            // 
+            // btn_BrowseOutput
+            // 
+            this.btn_BrowseOutput.Location = new System.Drawing.Point(310, 100);
+            this.btn_BrowseOutput.Name = "btn_BrowseOutput";
+            this.btn_BrowseOutput.Size = new System.Drawing.Size(75, 30);
+            this.btn_BrowseOutput.TabIndex = 5;
+            this.btn_BrowseOutput.Text = "Browse...";
+            this.btn_BrowseOutput.UseVisualStyleBackColor = true;
+            this.btn_BrowseOutput.Click += new System.EventHandler(this.btn_BrowseOutput_Click);
+            // 
+            // txtBox_Log
+            // 
+            this.txtBox_Log.Location = new System.Drawing.Point(12, 240);
+            this.txtBox_Log.Name = "txtBox_Log";
+            this.txtBox_Log.ReadOnly = true;
+            this.txtBox_Log.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtBox_Log.Size = new System.Drawing.Size(292, 106);
+            this.txtBox_Log.TabIndex = 10;
+            this.txtBox_Log.Text = "";
+            // 
+            // chkBox_ExtractPAC
+            // 
+            this.chkBox_ExtractPAC.AutoSize = true;
+            this.chkBox_ExtractPAC.Checked = true;
+            this.chkBox_ExtractPAC.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBox_ExtractPAC.Location = new System.Drawing.Point(12, 132);
+            this.chkBox_ExtractPAC.Name = "chkBox_ExtractPAC";
+            this.chkBox_ExtractPAC.Size = new System.Drawing.Size(194, 21);
+            this.chkBox_ExtractPAC.TabIndex = 6;
+            this.chkBox_ExtractPAC.Text = "Extract from PAC Archives";
+            this.chkBox_ExtractPAC.UseVisualStyleBackColor = true;
+            // 
+            // chkBox_SearchSubfolders
+            // 
+            this.chkBox_SearchSubfolders.AutoSize = true;
+            this.chkBox_SearchSubfolders.Checked = true;
+            this.chkBox_SearchSubfolders.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBox_SearchSubfolders.Location = new System.Drawing.Point(12, 159);
+            this.chkBox_SearchSubfolders.Name = "chkBox_SearchSubfolders";
+            this.chkBox_SearchSubfolders.Size = new System.Drawing.Size(224, 21);
+            this.chkBox_SearchSubfolders.TabIndex = 7;
+            this.chkBox_SearchSubfolders.Text = "Recursively Search Subfolders";
+            this.chkBox_SearchSubfolders.UseVisualStyleBackColor = true;
+            // 
+            // Form_PersonaTextDumper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 355);
+            this.Controls.Add(this.chkBox_SearchSubfolders);
+            this.Controls.Add(this.chkBox_ExtractPAC);
+            this.Controls.Add(this.txtBox_Log);
+            this.Controls.Add(this.lbl_OutputDir);
+            this.Controls.Add(this.txtBox_OutputDir);
+            this.Controls.Add(this.btn_BrowseOutput);
+            this.Controls.Add(this.chkBox_NameAfterContainers);
+            this.Controls.Add(this.chkBox_KeepFolderStructure);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_Type);
+            this.Controls.Add(this.lbl_SearchDir);
+            this.Controls.Add(this.txtBox_SearchDir);
+            this.Controls.Add(this.btn_BrowseSearch);
+            this.Controls.Add(this.btn_Dump);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_PersonaTextDumper";
+            this.Text = "Persona Texture Dumper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Dump;
+        private System.Windows.Forms.Button btn_BrowseSearch;
+        private System.Windows.Forms.TextBox txtBox_SearchDir;
+        private System.Windows.Forms.Label lbl_SearchDir;
+        private System.Windows.Forms.ComboBox comboBox_Type;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkBox_KeepFolderStructure;
+        private System.Windows.Forms.CheckBox chkBox_NameAfterContainers;
+        private System.Windows.Forms.Label lbl_OutputDir;
+        private System.Windows.Forms.TextBox txtBox_OutputDir;
+        private System.Windows.Forms.Button btn_BrowseOutput;
+        private System.Windows.Forms.RichTextBox txtBox_Log;
+        private System.Windows.Forms.CheckBox chkBox_ExtractPAC;
+        private System.Windows.Forms.CheckBox chkBox_SearchSubfolders;
+    }
+}
+
---
+++ PersonaTexDumper/PersonaTexDumper/PersonaTexDumperForm.cs
@@ -0,0 +1,279 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using AtlusFileSystemLibrary.FileSystems.PAK;
+using AtlusFileSystemLibrary.Common.IO;
+using AmicitiaLibrary.Graphics.RenderWare;
+using GFDLibrary;
+using GFDLibrary.Textures;
+using p5eplinjector;
+using AmicitiaLibrary.Graphics.TMX;
+using AmicitiaLibrary.Graphics.SPR;
+using AmicitiaLibrary.Graphics.SPD;
+
+namespace PersonaTexDumper
+{
+    public partial class Form_PersonaTextDumper : Form
+    {
+
+        public Form_PersonaTextDumper()
+        {
+            InitializeComponent();
+            comboBox_Type.SelectedIndex = 0;
+
+            #if DEBUG
+            txtBox_SearchDir.Text = @"C:\Users\Ryan\Downloads\RMD\Input";
+            txtBox_OutputDir.Text = @"C:\Users\Ryan\Downloads\RMD\Output";
+            #endif
+        }
+
+        private void btn_BrowseSearch_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txtBox_SearchDir.Text = dialog.FileName;
+            }
+        }
+
+        private void btn_BrowseOutput_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txtBox_OutputDir.Text = dialog.FileName;
+            }
+        }
+
+        private void btn_Dump_Click(object sender, EventArgs e)
+        {
+            txtBox_Log.Clear();
+            if (Directory.Exists(txtBox_SearchDir.Text))
+            {
+                Directory.CreateDirectory(txtBox_OutputDir.Text);
+                btn_Dump.Enabled = false; //Disallow pressing button again mid-operation
+                var searchOptions = chkBox_SearchSubfolders.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+                var files = Directory.GetFiles(txtBox_SearchDir.Text, "*", searchOptions);
+                if (comboBox_Type.Text != "Any")
+                {
+                    var allowedExtensions = new List<string>() { $".{comboBox_Type.Text}" };
+                    if (chkBox_ExtractPAC.Checked)
+                    {
+                        allowedExtensions.Add($".PAC");
+                        allowedExtensions.Add($".PAK");
+                        allowedExtensions.Add($".BIN");
+                        allowedExtensions.Add($".ARC");
+                    }
+                    files = files.Where(file => allowedExtensions.Any(file.ToUpper().EndsWith)).ToArray();
+                }
+                foreach (var file in files)
+                    Search(file);
+            }
+            else
+                txtBox_Log.Text += "Could not find Search Directory.\n";
+            txtBox_Log.Text += "Done!\n";
+            btn_Dump.Enabled = true;
+        }
+
+        private void Search(string file)
+        {
+            try
+            {
+                switch (Path.GetExtension(file).ToUpper())
+                {
+                    case (".PAC"):
+                        ExtractPAC(file);
+                        break;
+                    case (".PAK"):
+                        ExtractPAC(file);
+                        break;
+                    case (".BIN"):
+                        ExtractPAC(file);
+                        break;
+                    case (".ARC"):
+                        ExtractPAC(file);
+                        break;
+                    case (".RMD"):
+                        ExtractRMD(file);
+                        break;
+                    case (".GMD"):
+                        ExtractGMD(file);
+                        break;
+                    case (".GMO"):
+                        //ExtractGMO(file);
+                        break;
+                    case (".EPL"):
+                        ExtractEPL(file);
+                        break;
+                    case (".CPK"):
+                        //ExtractCPK(file);
+                        break;
+                    case (".SPR"):
+                        ExtractSPR(file);
+                        break;
+                    case (".SPD"):
+                        ExtractSPD(file);
+                        break;
+                    case (".TMX"):
+                        ExtractTMX(file);
+                        break;
+                }
+            }
+            catch
+            {
+                txtBox_Log.Text += $"Failed to load {Path.GetFileName(file)}.\n";
+            }
+            
+        }
+
+        private void ExtractSPD(string file)
+        {
+            SpdFile spd = new SpdFile(file);
+            foreach (var texture in spd.Textures)
+            {
+                string output = txtBox_OutputDir.Text;
+                if (chkBox_KeepFolderStructure.Checked)
+                    output += Path.DirectorySeparatorChar + Path.GetDirectoryName(file).Substring(txtBox_SearchDir.Text.Length);
+                if (chkBox_NameAfterContainers.Checked)
+                    output += Path.DirectorySeparatorChar + Path.GetFileName(file) + "_extracted";
+                Directory.CreateDirectory(output);
+
+                var bitmap = texture.GetBitmap();
+                bitmap.Save(output + Path.DirectorySeparatorChar + Path.ChangeExtension(texture.Description, "png"));
+                txtBox_Log.Text += $"Saving texture: {texture.Description}\n";
+            }
+        }
+
+        private void ExtractSPR(string file)
+        {
+            SprFile spr = SprFile.Load(file);
+            foreach (var texture in spr.Textures)
+            {
+                string output = txtBox_OutputDir.Text;
+                if (chkBox_KeepFolderStructure.Checked)
+                    output += Path.DirectorySeparatorChar + Path.GetDirectoryName(file).Substring(txtBox_SearchDir.Text.Length);
+                if (chkBox_NameAfterContainers.Checked)
+                    output += Path.DirectorySeparatorChar + Path.GetFileName(file) + "_extracted";
+                Directory.CreateDirectory(output);
+
+                var bitmap = texture.GetBitmap();
+                bitmap.Save(output + Path.DirectorySeparatorChar + Path.ChangeExtension(texture.UserComment, "png"));
+                txtBox_Log.Text += $"Saving texture: {texture.UserComment}\n";
+            }
+        }
+
+        private void ExtractTMX(string file)
+        {
+            string output = txtBox_OutputDir.Text;
+            if (chkBox_KeepFolderStructure.Checked)
+                output += Path.DirectorySeparatorChar + Path.GetDirectoryName(file).Substring(txtBox_SearchDir.Text.Length);
+            if (chkBox_NameAfterContainers.Checked)
+                output += Path.DirectorySeparatorChar + Path.GetFileName(file) + "_extracted";
+            Directory.CreateDirectory(output);
+
+            TmxFile tmx = new TmxFile(file);
+            
+            var bitmap = tmx.GetBitmap();
+            bitmap.Save(output + Path.DirectorySeparatorChar + Path.ChangeExtension(Path.GetFileName(file), "png"));
+            txtBox_Log.Text += $"Saving texture: {Path.GetFileName(file)}\n";
+        }
+
+        private void ExtractEPL(string file)
+        {
+            txtBox_Log.Text += $"Extracting {Path.GetFileName(file)}...\n";
+            string output = txtBox_OutputDir.Text;
+            if (chkBox_KeepFolderStructure.Checked)
+                output += Path.DirectorySeparatorChar + Path.GetDirectoryName(file).Substring(txtBox_SearchDir.Text.Length);
+            Directory.CreateDirectory(output);
+            string copiedFile = output + Path.DirectorySeparatorChar + Path.GetFileName(file);
+            File.Copy(file, copiedFile);
+
+            Epl.ExtractModelsFromEpl(copiedFile);
+            File.Delete(copiedFile);
+
+            foreach (var gmd in Directory.GetFiles(output, "*.gmd", SearchOption.AllDirectories))
+                Search(gmd);
+        }
+
+        private void ExtractGMD(string file)
+        {
+            var gmd = Resource.Load<ModelPack>(file);
+            if (gmd.Textures != null)
+            foreach (var texture in gmd.Textures.Textures)
+            {
+                string output = txtBox_OutputDir.Text;
+                if (chkBox_KeepFolderStructure.Checked)
+                    output += Path.DirectorySeparatorChar + Path.GetDirectoryName(file).Substring(txtBox_SearchDir.Text.Length);
+                if (chkBox_NameAfterContainers.Checked)
+                    output += Path.DirectorySeparatorChar + Path.GetFileName(file) + "_extracted";
+                Directory.CreateDirectory(output);
+
+                var bitmap = TextureDecoder.Decode(texture);
+                bitmap.Save(output + Path.DirectorySeparatorChar + Path.ChangeExtension(texture.Name, "png"));
+                txtBox_Log.Text += $"Saving texture: {texture.Name}\n";
+            }
+        }
+
+        private void ExtractRMD(string file)
+        {
+            RmdScene rmd = new RmdScene(file);
+            foreach (var texture in rmd.TextureDictionary.Textures)
+            {
+                string output = txtBox_OutputDir.Text;
+                if (chkBox_KeepFolderStructure.Checked)
+                    output += Path.DirectorySeparatorChar + Path.GetDirectoryName(file).Substring(txtBox_SearchDir.Text.Length + 1);
+                if (chkBox_NameAfterContainers.Checked)
+                    output += Path.DirectorySeparatorChar + Path.GetFileName(file) + "_extracted";
+                Directory.CreateDirectory(output);
+
+                var bitmap = texture.GetBitmap();
+                bitmap.Save(output + Path.DirectorySeparatorChar + Path.ChangeExtension(texture.Name, "png"));
+                txtBox_Log.Text += $"Saving texture: {texture.Name}\n";
+            }
+        }
+
+        private void ExtractPAC(string file)
+        {
+            PAKFileSystem pak = new PAKFileSystem();
+            if (PAKFileSystem.TryOpen(file, out pak))
+            {
+                List<string> pakFiles = new List<string>();
+                txtBox_Log.Text += $"Extracting {Path.GetFileName(file)}...\n";
+                foreach (var pakFile in pak.EnumerateFiles())
+                {
+                    var normalizedFilePath = pakFile.Replace("../", ""); // Remove backwards relative path
+                    string output = txtBox_OutputDir.Text;
+                    if (chkBox_KeepFolderStructure.Checked)
+                        output += Path.DirectorySeparatorChar + Path.GetDirectoryName(file).Substring(txtBox_SearchDir.Text.Length);
+                    if (chkBox_NameAfterContainers.Checked)
+                        output += Path.DirectorySeparatorChar + Path.GetFileName(file) + "_extracted" + Path.DirectorySeparatorChar + normalizedFilePath;
+                    else
+                        output += Path.DirectorySeparatorChar + normalizedFilePath;
+
+                    using (var stream = FileUtils.Create(output))
+                    using (var inputStream = pak.OpenFile(pakFile))
+                    {
+                        inputStream.CopyTo(stream);
+                        pakFiles.Add(output);
+                    }
+                    File.Delete(pakFile);
+                }
+                
+                foreach (var pakFile in pakFiles)
+                {
+                    Search(pakFile);
+                }
+            }
+            else
+            {
+                txtBox_Log.Text += $"Failed to open. Skipping...\n";
+            }
+        }
+    }
+}
